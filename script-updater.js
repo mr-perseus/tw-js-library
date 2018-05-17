9744cd88a933133d8290d651810e126cd095fdc9
Jan <Jan.Frei@eps.ch>
2018-05-17 15:58:32 +0200

    Initial Commit

---
+++ script-updater.js
@@ -0,0 +1,8 @@
+var scriptUpdater = {
+    TWDW: '0.2.1',
+    TWDWNew: 'Performance improvements',
+    TWCAO: '0.0.3',
+    TWCAONew: 'Added a dialog which asks whether you want to open the alliance chat or not.',
+    TWDCC: '0.0.1-beta-2',
+    TWDCCNew: 'Update link added.'
+};
