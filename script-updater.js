85e20d824079c1de07ec9a2e2ec82ddcbff89a84
mr-perseus <38375248+mr-perseus@users.noreply.github.com>
2018-07-08 15:06:14 +0200

    Update script-updater.js

--- script-updater.js
+++ script-updater.js
@@ -4,5 +4,7 @@ var scriptUpdater = {
     TWCAO: '0.0.3',
     TWCAONew: 'Added a dialog which asks whether you want to open the alliance chat or not.',
     TWDCC: '0.0.1-beta-2',
-    TWDCCNew: 'Update link added.'
+    TWDCCNew: 'Update link added.',
+    TWPT: '0.1.0',
+    TWPTNew: 'First version'
 };
