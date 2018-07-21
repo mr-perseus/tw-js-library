fd427ee4845505706a0a9d587cb4b89dcd1ef271
mr-perseus <38375248+mr-perseus@users.noreply.github.com>
2018-07-21 15:15:15 +0200

    Update script-updater.js

--- script-updater.js
+++ script-updater.js
@@ -1,6 +1,6 @@
 var scriptUpdater = {
-    TWDW: '0.3.0',
-    TWDWNew: 'New feature: Watched Players',
+    TWDW: '0.3.1',
+    TWDWNew: 'Added option to watch all players',
     TWCAO: '0.0.3',
     TWCAONew: 'Added a dialog which asks whether you want to open the alliance chat or not.',
     TWDCC: '0.0.1-beta-2',
