e36e1d8ea133698ba4ccf4dc822ce3db86574dd7
mr-perseus <38375248+mr-perseus@users.noreply.github.com>
2018-08-25 15:12:24 +0200

    Update script-updater.js

--- script-updater.js
+++ script-updater.js
@@ -5,6 +5,6 @@ var scriptUpdater = {
     TWCAONew: 'Added a dialog which asks whether you want to open the alliance chat or not.',
     TWDCC: '0.0.1-beta-2',
     TWDCCNew: 'Update link added.',
-    TWPT: '0.1.1',
-    TWPTNew: 'Settings added <a target="_blank" href="https://greasyfork.org/scripts/370137-the-west-perseus-toolkit/code/The%20West%20Perseus%20Toolkit.user.js">PLEASE CLICK HERE TO UPDATE</a> (update button is linked wrongly, sorry about that)'
+    TWPT: '0.2.0',
+    TWPTNew: 'New features: Zoom map, disable premium notifications.'
 };
