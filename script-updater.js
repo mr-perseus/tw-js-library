e4289c37406545f74dcfdeb1087319971b985bea
mr-perseus <38375248+mr-perseus@users.noreply.github.com>
2019-06-08 17:57:46 +0200

    Update script-updater.js

--- script-updater.js
+++ script-updater.js
@@ -1,6 +1,6 @@
 var scriptUpdater = {
-    TWDW: '0.3.1',
-    TWDWNew: 'Added option to watch all players',
+    TWDW: '1.0.0',
+    TWDWNew: 'New feature: Fire a system notification if a player moves to your position / if a player on your watch list gets attackable.',
     TWCAO: '0.0.3',
     TWCAONew: 'Added a dialog which asks whether you want to open the alliance chat or not.',
     TWDCC: '0.0.1-beta-2',
