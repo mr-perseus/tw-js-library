3442d21630c98baa104cf1e5e9e86d2414cb00f0
mr-perseus <38375248+mr-perseus@users.noreply.github.com>
2020-02-11 17:41:16 +0100

    Update script-updater.js

--- script-updater.js
+++ script-updater.js
@@ -5,8 +5,8 @@ var scriptUpdater = {
     TWCAONew: 'Added a dialog which asks whether you want to open the alliance chat or not.',
     TWDCC: '0.0.1-beta-2',
     TWDCCNew: 'Update link added.',
-    TWPT: '1.0.0',
-    TWPTNew: 'New feature: Hide "Drawing Map" flag.',
+    TWPT: '1.0.1',
+    TWPTNew: 'New feature: Chat improvements.',
     TWPTExt: '1.0.0',
     TWPTExtNew: 'New feature: Hide "Drawing Map" flag.'
 };
