f5b2157d7a4d364df7db7bbb4e1d8eb84f9c7fa1
mr-perseus <38375248+mr-perseus@users.noreply.github.com>
2020-09-26 12:44:12 +0000

    Update script-updater.js

--- script-updater.js
+++ script-updater.js
@@ -5,8 +5,8 @@ var scriptUpdater = {
     TWCAONew: 'Added a dialog which asks whether you want to open the alliance chat or not.',
     TWDCC: '1.0.0',
     TWDCCNew: 'New Initial Release.',
-    TWPT: '1.0.1',
-    TWPTNew: 'New feature: Chat improvements.',
+    TWPT: '1.1.0',
+    TWPTNew: 'Remove job highlighter to comply with the rules',
     TWPTExt: '99',
     TWPTExtNew: 'THIS SCRIPT IS DISCONTINUED! Please migrate to "The West Perseus Toolkit" and remove "The West Perseus Toolkit Extended".'
 };
